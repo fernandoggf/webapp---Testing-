b9a5cb2783e8a41e368194822e44b839bfeb7f0b
fernandofig <fernando.ggfigueroa@icloud.com>
2022-02-11 23:02:18 -0600

    Forms from the web service
    Ready to use the booleanField

--- app_debug.py
+++ app_debug.py
@@ -1,11 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
-
+from forms import values_debug
 
 app = Flask(__name__, template_folder = 'temp')
 
 
 
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+app.config['SECRET_KEY'] = 'sfsf31ddqDEWD23'
 
 
 
@@ -15,10 +16,15 @@ def home():
 	return render_template("home.html", title="HomePage")
 
 
-@app.route('/values_sub', methods=["POST", "GET"])
+@app.route("/values_sub", methods=["GET", "POST"])
 def values_sub():
-	return render_template("values_sub.html", title="Debug Values")
-
+	form = values_debug()
+	if form.validate_on_submit():
+		print(form.debug.data)
+		if form.debug.data == True:
+			print("y")
+		return redirect(url_for('home'))
+	return render_template("values_sub.html", title="Debug Values", form=form)
 
 
 if __name__ == "__main__":
