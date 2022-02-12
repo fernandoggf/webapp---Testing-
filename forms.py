d042331f871c6bc0def60a8b7a23db05287ded45
fernandofig <fernando.ggfigueroa@icloud.com>
2022-02-11 23:01:43 -0600

    Forms to GET and POST
    Learning to use forms from flask_wtf, handling GET and POST, and using a checkbox to get the debug Value

--- forms.py
+++ forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField, SelectField, BooleanField 
+from wtforms import validators, ValidationError
+from wtforms.validators import DataRequired
 
 #Python classes will be the representatives of the HTML forms, flask will convert them
 
 class values_debug(FlaskForm):
-	pass
+	url_given = StringField('URL Address (optional)')
+	debug = BooleanField('Turn-on Debug?', default=0, validators=[DataRequired(), ])
+	submit = SubmitField('ok!')
+	
